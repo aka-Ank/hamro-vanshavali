b28c948ec4b4bb63d832ebf929fa5504b49b6c5e
sachit.razz__ <sachitrchaudhary@gmail.com>
2026-05-26 19:37:39 +0545

    Built homepage UI with rougint and Tailwind Styling

---
+++ frontend/src/data/FamilyData.ts
@@ -0,0 +1,16 @@
+import type { Family } from "../types/Family";
+
+export const family: Family = {
+  id: 1,
+
+  familyName: "Thapa",
+
+  subdomain: "thapa",
+
+  description:
+    "Preserving the lineage, history, and heritage of the Thapa family.",
+
+  totalGenerations: 3,
+
+  totalMembers: 4,
+};
---
+++ frontend/src/types/family.ts
@@ -0,0 +1,13 @@
+export type Family = {
+  id: number;
+
+  familyName: string;
+
+  subdomain: string;
+
+  description: string;
+
+  totalGenerations: number;
+
+  totalMembers: number;
+};
